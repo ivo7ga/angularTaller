b84142425e3f743add872dc0ce736dff9870c7c3
ivo7ga <ivan_gonzalez_tsapp1oa2021@cev.com>
2022-03-02 16:38:25 +0100

    Fotos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
+import { ServicesModule } from './services/services.module';
+import { InterceptorsModule } from './interceptors/interceptors.module';
+import { GuardsModule } from './guards/guards.module';
+import { ComponentsModule } from './components/components.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +17,12 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
+    ComponentsModule,
+    GuardsModule,
+    InterceptorsModule,
+    ServicesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/components.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ImagesComponent } from './images/images.component';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [
+    ImagesComponent
+  ],
+  imports: [
+    CommonModule,
+    NgbCarouselModule, // Empaquetado el carrusel
+    FormsModule        // Controles de formulario
+
+  ]
+})
+export class ComponentsModule { }
